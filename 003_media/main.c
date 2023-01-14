89296dd36513712aee8064d40f65b28d44a95029
Marcos Fernando <estudogit@outlook.com>
2023-01-14 17:00:04 -0300

    media

---
+++ 003_media/main.c
@@ -0,0 +1,25 @@
+// #include <stdio.h>
+
+// #define N 3
+
+// float media(int *vet) {
+//     float soma = 0;
+//     for (int i = 0; i < N; i++) {
+//         soma += vet[i];
+//     }
+//     return soma / N;
+// }
+
+// int main(void) {
+//     int vet[N];
+
+//     for (int i = 0; i < N; i++) {
+//         printf("Numero %d: ", i + 1);
+//         scanf("%d", &vet[i]);
+//     }
+    
+//     printf("Media: %.2f", media(&vet));
+    
+    
+//     return 0;
+// }
