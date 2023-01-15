1f7bad30d5ceda86ff9c912bb028a8f0cefadf72
Marcos Fernando <estudogit@outlook.com>
2023-01-15 12:34:06 -0300

    atualizacao no readme

--- 005_vetor_com_malloc/main.c
+++ 005_vetor_com_malloc/main.c
@@ -1,21 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-#define N 3
+// #define N 3
 
-int main(void) {
-    int *vet, i;
+// int main(void) {
+//     int *vet, i;
     
-    vet = (int*)malloc(N * sizeof(int));
+//     vet = (int*)malloc(N * sizeof(int));
     
-    for (i = 0; i < N; i++) {
-        printf("Numero %d: ", i + 1);
-        scanf("%d", &vet[i]);
-    }
+//     for (i = 0; i < N; i++) {
+//         printf("Numero %d: ", i + 1);
+//         scanf("%d", &vet[i]);
+//     }
     
-    for (i = 0; i < N; i++) {
-        printf("%d ", vet[i]);
-    }
-    
-    return 0;
-}
+//     for (i = 0; i < N; i++) {
+//         printf("%d ", vet[i]);
+//     }
+
+//     free(vet);
+
+//     return 0;
+// }
