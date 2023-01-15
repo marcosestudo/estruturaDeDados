a4f767c20c8906e074c8c36589d9a759cfdd1130
Marcos Fernando <estudogit@outlook.com>
2023-01-15 17:22:07 -0300

    lista encadeada

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct lista {
+  	int info;
+	struct lista *prox;
+};
+typedef struct lista TLista;
+
+TLista* cria_lista(void) {
+	return NULL;
+}
+
+int main(void) {
+	Tlista *L;
+
+	L = cria_lista;
+    
+  	return 0;
+}
