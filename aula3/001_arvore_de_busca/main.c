2996274e2e6f66c40cd390c910931f329a0f9376
Marcos Fernando <estudogit@outlook.com>
2023-01-17 22:30:22 -0300

    busca

--- aula3/001_arvore_de_busca/main.c
+++ aula3/001_arvore_de_busca/main.c
@@ -7,3 +7,19 @@ typedef struct sNoA {
     struct sNoA *esq;
     struct sNoA *dir;
 } TNoA;
+
+TNoA *busca(TNoA *no, int chave) {
+    // recebe endereço da raiz e chave procurada
+    while (no != NULL) {
+        if (no->chave == chave) {
+            return no; // achou, retorna o ponteiro pro nó
+        } else {
+            if (no->chave > chave) {
+                no = no->esq;
+            } else {
+                no = no->dir;
+            }
+        }
+    }
+    return NULL; // não achou, retorna null
+}
